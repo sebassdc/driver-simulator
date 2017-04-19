3e094c67b4ae0d03bc2c6522b7efb9bae02bfd43
Sebastian Hurtado <sebassdc@gmail.com>
2017-04-18 21:31:03 -0400

    Working!!!!

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import './helpers/context_menu.js';
 import './helpers/external_links.js';
 
 // All stuff below is just to show you how it works. You can delete all of it.
-import { remote } from 'electron';
+import { remote, ipcRenderer as ipc } from 'electron';
 import jetpack from 'fs-jetpack';
 import { greet } from './hello_world/hello_world';
 import env from './env';
@@ -13,18 +13,14 @@ import env from './env';
 const app = remote.app;
 const appDir = jetpack.cwd(app.getAppPath());
 
+
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // here files form disk like it's node.js! Welcome to Electron world :)
-const manifest = appDir.read('package.json', 'json');
 
-const osMap = {
-  win32: 'Windows',
-  darwin: 'macOS',
-  linux: 'Linux',
-};
+document.getElementById('create_device').addEventListener("click", () => {
+  ipc.send('create_device', 'ping');
+});
 
-document.querySelector('#greet').innerHTML = greet();
-document.querySelector('#os').innerHTML = osMap[process.platform];
-document.querySelector('#author').innerHTML = manifest.author;
-document.querySelector('#env').innerHTML = env.name;
-document.querySelector('#electron-version').innerHTML = process.versions.electron;
+document.getElementById('connect_device').addEventListener("click", () => {
+  ipc.send('connect_device', 'ping');
+});
--- src/background.js
+++ src/background.js
@@ -5,50 +5,154 @@
 
 import path from 'path';
 import url from 'url';
-import { app, Menu } from 'electron';
-import { devMenuTemplate } from './menu/dev_menu_template';
-import { editMenuTemplate } from './menu/edit_menu_template';
+const crypto = require('crypto');
+import fs from 'fs';
+import {
+	app,
+	Menu,
+	ipcMain as ipc,
+	dialog
+} from 'electron';
+import {
+	devMenuTemplate
+} from './menu/dev_menu_template';
+import {
+	editMenuTemplate
+} from './menu/edit_menu_template';
 import createWindow from './helpers/window';
+import jetpack from 'fs-jetpack';
 
 // Special module holding environment variables which you declared
 // in config/env_xxx.json file.
 import env from './env';
 
 const setApplicationMenu = () => {
-  const menus = [editMenuTemplate];
-  if (env.name !== 'production') {
-    menus.push(devMenuTemplate);
-  }
-  Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
+	const menus = [editMenuTemplate];
+	if (env.name !== 'production') {
+		menus.push(devMenuTemplate);
+	}
+	Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
 };
 
 // Save userData in separate folders for each environment.
 // Thanks to this you can use production and development versions of the app
 // on same machine like those are two separate apps.
 if (env.name !== 'production') {
-  const userDataPath = app.getPath('userData');
-  app.setPath('userData', `${userDataPath} (${env.name})`);
+	const userDataPath = app.getPath('userData');
+	app.setPath('userData', `${userDataPath} (${env.name})`);
 }
 
 app.on('ready', () => {
-  setApplicationMenu();
+	setApplicationMenu();
 
-  const mainWindow = createWindow('main', {
-    width: 1000,
-    height: 600,
-  });
+	const mainWindow = createWindow('main', {
+		width: 400,
+		height: 600,
+	});
 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'app.html'),
-    protocol: 'file:',
-    slashes: true,
-  }));
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'app.html'),
+		protocol: 'file:',
+		slashes: true,
+	}));
 
-  if (env.name === 'development') {
-    mainWindow.openDevTools();
-  }
+	if (env.name === 'development') {
+		mainWindow.openDevTools();
+	}
 });
 
 app.on('window-all-closed', () => {
-  app.quit();
+	app.quit();
+});
+
+let creatorWindow;
+ipc.on('create_device', (event, arg) => {
+	creatorWindow = createWindow('creator', {
+		width: 400,
+		height: 600,
+	});
+	creatorWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'device_creator.html'),
+		protocol: 'file:',
+		slashes: true,
+	}));
+});
+
+ipc.on('open-error-dialog', (event, arg) => {
+	dialog.showErrorBox('ERROR', arg);
+});
+
+ipc.on('genera-dispositivo', (event, arg) => {
+	const options = {
+		title: 'Selecciona un directorio para guardar tu dispositivo',
+		filters: [
+			{
+				name: "Json",
+				extensions: ['json']
+			}
+    ]
+	}
+	dialog.showSaveDialog(options, (filename) => {
+		jetpack.write(filename, arg);
+		let algoritmo = 'md5';
+		let shasum = crypto.createHash(algoritmo);
+		let s = jetpack.read(filename);
+		shasum.update(s)
+		let d = shasum.digest('hex');
+		dialog.showSaveDialog({
+			title: 'Selecciona un directorio para guardar tu driver',
+			filters: [
+				{
+					name: "Json",
+					extensions: ['json']
+				}
+      ]
+		}, (filename) => {
+			jetpack.write(filename, Object.assign({
+				checksum: d
+			}, arg));
+		});
+	});
+});
+
+let conectorWindow;
+ipc.on('connect_device', (event, arg) => {
+	conectorWindow = createWindow('conector', {
+		width: 400,
+		height: 600,
+	});
+	conectorWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'device_conector.html'),
+		protocol: 'file:',
+		slashes: true,
+	}));
+});
+
+ipc.on('conectar-dispositivo', (event, arg) => {
+  console.log(arg.devicePath)
+	let device = jetpack.read(arg.device);
+	let driver = jetpack.read(arg.driver, 'json');
+
+	// Prueba si son iguales
+	let algoritmo = 'md5';
+	let shasum = crypto.createHash(algoritmo);
+	shasum.update(device)
+	let d = shasum.digest('hex');
+	if (d == driver["checksum"]) {
+		const options = {
+			type: 'info',
+			title: 'Conectado!',
+			message: `¡Se ha instalado el driver y conectado el dispositivo!`
+		}
+    dialog.showMessageBox(options);
+	} else {
+		const options = {
+			type: 'info',
+			title: 'Parece que no es el driver:(',
+			message: `Contacta con el fabricante del dispositivo`
+		}
+    dialog.showMessageBox(options);
+	}
+
+
 });
